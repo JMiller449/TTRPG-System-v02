245a1665695bdac3c9fb836df03c27064929ddce
Joseph Miller <millerjoseph449@gmail.com>
2026-01-19 01:25:15 -0600

    Update main.py

--- backend/core/main.py
+++ backend/core/main.py
@@ -1,15 +1,28 @@
-# Handles app creation, router initialization, and startup/shutdown
-
+from dataclasses import dataclass
 import json
-from typing import Union
+from typing import Literal, Union
 from fastapi import FastAPI, WebSocket
 
 app = FastAPI()
 
-# TODO: make this specific to a union of objects
-UserRequests = Union[str]
 
-# TODO:
+# TODO: make this specific to a union of objects
+@dataclass
+class Error:
+    type: Literal["error"] = "error"
+    message: str
+
+
+Resposnes = Union[Error]
+
+
+@dataclass
+class Yap:
+    type: Literal["yap"] = "yap"
+    message: str
+
+
+Requests = Union[Yap]
 
 
 @app.websocket("/ws")
@@ -18,8 +31,11 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         data = await websocket.receive_text()
         try:
-            req_obj: UserRequests = json.load(data)
-        except Error:
+            req_obj: Requests = json.load(data)
+            match (req_obj.type):
+                case "yap":
+                    print(f"Yapper said {req_obj.message}")
+        except:
             websocket.send_text(f"Invalid Request: {data}")
             return
         await websocket.send_text(f"Message text was: {data}")

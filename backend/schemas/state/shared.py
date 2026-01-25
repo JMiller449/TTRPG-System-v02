177f4e952a0f9f9d0e4c3b7ccf75f54a4a70790e
Joseph Miller <millerjoseph449@gmail.com>
2026-01-25 17:47:32 -0600

    commenting Bridge attributes

--- backend/schemas/state/shared.py
+++ backend/schemas/state/shared.py
@@ -3,5 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Bridge:
-    relationship_id: str
-    entry_id: str
+    relationship_id: str # an ID to reference this Bridge
+    entry_id: str # points to a dataclass with the info being 'bridged to'

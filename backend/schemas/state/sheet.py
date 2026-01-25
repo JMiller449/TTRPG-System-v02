270fb500384ac3d5151b1652d649929b489ad9f3
Joseph Miller <millerjoseph449@gmail.com>
2026-01-25 17:21:07 -0600

    Update sheet.py
    split intancesheet into PersitantSheet and CombatSheet to separate values needed in/outside of combat.
    
    CombatSheet references CombatSheet

--- backend/schemas/state/sheet.py
+++ backend/schemas/state/sheet.py
@@ -7,29 +7,39 @@ from backend.schemas.state.stat import Stats
 
 
 @dataclass
-class SheetSlainedBridge:
+class SheetSlayedBridge:
     sheet_id: str
     count: int
 
 
-@dataclass
-class InstancedSheet:
-    parent_id: str
-    active: bool  # toggle for dm to hide goblins in the bush or character off screen
-    health: float
-    mana: int
-    action_count: int
-
-
+# Static sheet, contains basic data like max health, base statistics, max actions, etc.
 @dataclass
 class Sheet:
     id: str
     name: str
     dm_only: bool  # toogle to hide from other users
-    xp_given_when_slained: int
+    xp_given_when_slayed: int
     xp_cap: str
     proficiencies: Dict[str, Proficiency]
     items: Dict[str, ItemBridge]
     stats: Stats
-    slained_record: Dict[str, SheetSlainedBridge]
+    slayed_record: Dict[str, SheetSlayedBridge]
     actions: Dict[str, Bridge]
+
+
+# an instanced persistant sheet, contains current values and augmented stat values (i.e. current health/mana)
+@dataclass
+class PersistantSheet:
+    parent_id: str # points to parent Sheet
+    health: float
+    mana: int
+    augments: Dict[str, Bridge] # TODO add augments dict
+
+
+# Holds combat specific information that does not need to be stored outside of combat or initiative
+@dataclass
+class CombatSheet:
+    parent_id: str # points to parent PersistantSheet
+    active: bool  # toggle for dm to hide goblins in the bush or character off screen
+    action_count: int # remaining actions
+    initiative: int # the initiative value rolled to determine initiative order
